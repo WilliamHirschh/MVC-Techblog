2a77ea0d15f9d2c0fec6592ba2a88c83b5e03e13
William Hirsch <Williamhirschh@gmail.com>
2023-02-09 12:16:38 -0600

    had to swap code since i wrote it in wrong place and then got all the routes set up

---
+++ controllers/api/index.js
@@ -0,0 +1,15 @@
+//grabs
+
+const router = require('express').Router();
+const blogRoutes = require('./blogRoutes');
+const commentRoutes = require('./commentRoutes');
+const userRoutes = require('./userRoutes');
+
+
+router.use('/blogs', blogRoutes);
+router.use('/comments', commentRoutes);
+router.use('/users', userRoutes);
+
+
+//router export
+module.exports = router;
